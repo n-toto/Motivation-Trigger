44eb6997b1e5a3b7c35e88e63fc721d6cdf97494
Naho <t.naho.1737@gmail.com>
2021-11-07 12:42:05 +0900

    Set initial route by notification

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'views/homepage.dart';
+import 'views/question_top.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   var initializationSettingsAndroid =
@@ -27,7 +28,31 @@ void main() async {
       debugPrint('notification payload: ' + payload);
     }
   });
-  runApp(MyApp());
+  final NotificationAppLaunchDetails notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+  final didNotificationLaunchApp =
+      notificationAppLaunchDetails?.didNotificationLaunchApp ?? false;
+  String initialRoute = didNotificationLaunchApp ? '/questions' : '/homepage';
+  runApp(
+    MaterialApp(
+      title: 'Motivation Trigger',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: initialRoute,
+      routes: <String, WidgetBuilder>{
+        '/homepage': (context) => ChangeNotifierProvider<MenuInfo>(
+              create: (context) => MenuInfo(MenuType.clock),
+              child: HomePage(),
+            ),
+        '/questions': (context) => Start(),
+      },
+      // home: ChangeNotifierProvider<MenuInfo>(
+      //   create: (context) => MenuInfo(MenuType.clock),
+      //   child: HomePage(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -40,10 +65,17 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChangeNotifierProvider<MenuInfo>(
-        create: (context) => MenuInfo(MenuType.clock),
-        child: HomePage(),
-      ),
+      initialRoute: '/homepage',
+      routes: <String, WidgetBuilder>{
+        '/homepage': (context) => ChangeNotifierProvider<MenuInfo>(
+              create: (context) => MenuInfo(MenuType.clock),
+              child: HomePage(),
+            ),
+        '/questions': (context) => Start(),
+      },
+      // home: ChangeNotifierProvider<MenuInfo>(
+      //   create: (context) => MenuInfo(MenuType.clock),
+      //   child: HomePage(),
     );
   }
 }
